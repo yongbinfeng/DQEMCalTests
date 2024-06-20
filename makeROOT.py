25345ad04cf9b751bcbfe9cb2fb2853133d5023d
yongbinfeng <ybfeng94@gmail.com>
2024-06-19 21:12:26 -0500

    update

---
+++ makeROOT.py
@@ -0,0 +1,97 @@
+import sys, os
+sys.argv.append( '-b' )
+from ROOT import *
+from array import array
+
+gROOT.SetBatch(True)
+
+chlist = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+chlist_str = ["00","01","02","03","04","05","06","07","08","09","10","11","12","13","14","15"]
+ch_calib = [203.7,
+            190.8,
+            192.3,
+            201.1,
+            199.1,
+            207.2,
+            196.2,
+            218.2,
+            199.0,
+            201.0,
+            203.7,
+            203.7,
+            192.9,
+            187.7,
+            186.5,
+            181.5
+            ]
+calib_mean = sum(ch_calib)/len(ch_calib)
+
+def makeROOT(run):
+  print(f"Making ROOT file for Run {run}")
+  trigID = array('i', [0])
+  trigTime = array('d', [0])
+  ch_lg = std.vector[int]()
+  ch_hg = std.vector[int]()
+  ch_lg_calib = std.vector[float]()
+
+  fout = TFile(f"root/Run{run}_list.root","RECREATE")
+  tout = TTree("save","save")
+  tout.SetDirectory(fout)
+
+  tout.Branch('trigID', trigID, 'trigID/I')
+  tout.SetBranchAddress("trigID", trigID)
+
+  tout.Branch('trigTime', trigTime, 'trigTime/D')
+  tout.SetBranchAddress("trigTime", trigTime)
+
+  tout.Branch('ch_lg', ch_lg)
+  tout.SetBranchAddress("ch_lg",ch_lg)
+
+  tout.Branch('ch_hg', ch_hg)
+  tout.SetBranchAddress("ch_hg",ch_hg)
+
+  tout.Branch('ch_lg_calib', ch_lg_calib)
+  tout.SetBranchAddress("ch_lg_calib",ch_lg_calib)
+
+  nevents=0
+  
+  # check if file exists
+  if not os.path.exists(f"data/Run{run}_list.txt"):
+    print(f"File data/Run{run}_list.txt does not exist")
+    return
+  
+  with open(f"data/Run{run}_list.txt") as infile:
+
+    for line in infile:
+
+      if ("//" in line): continue
+      if ("Tstamp" in line): continue
+
+      if ("1  00" in line and len(line.split())==7):
+        trigID[0] = int(line.split()[5])
+        trigTime[0] = float(line.split()[4])
+
+      for ch in chlist:
+        if ("1  "+chlist_str[ch] in line):
+          ch_lg.push_back(int(line.split()[2]))
+          ch_hg.push_back(int(line.split()[3]))        
+          ch_lg_calib.push_back(int(line.split()[2])*ch_calib[ch]/calib_mean)
+
+      if (ch_lg.size()==len(chlist)):
+        tout.Fill()
+        ch_lg.clear()
+        ch_hg.clear()
+        ch_lg_calib.clear()
+        nevents+=1
+        if (nevents%1000==0): print(nevents)
+
+  fout.cd()
+  tout.Write()
+  fout.Save()
+  
+  print(f"Total events: {nevents}")
+  print(f"ROOT file saved as root/Run{run}_list.root")
+  print("\n\n")
+  
+for run in range(350, 359):
+  makeROOT(run)
---
+++ runFits.py
@@ -0,0 +1,107 @@
+import ROOT
+import sys
+
+fname = "h_lgcl0_Run347.root"
+#fname = "h_lgcl0_Run347_new4.root"
+f = ROOT.TFile(fname)
+h = f.Get("h_lgcl0")
+
+xmin = h.GetXaxis().GetXmin()
+xmax = h.GetXaxis().GetXmax()
+xmean = h.GetMean()
+xrms  = h.GetRMS()
+xbins = h.GetNbinsX()
+
+print("xmin = ", xmin)
+print("xmax = ", xmax)
+print("xmean = ", xmean)
+print("xrms = ", xrms)
+print("xbins = ", xbins)
+
+xfitmin = 2800
+xfitmax = 3400
+
+var = ROOT.RooRealVar( "energy", "energy", xmin, 7500, "ADCCount")
+#var.setBins(xbins)
+var.setRange("r1", xfitmin, xfitmax)
+datahist = ROOT.RooDataHist("datahist", "datahist", ROOT.RooArgList(var, "argdatahist"), h)
+
+mean = ROOT.RooRealVar("mean",  "mean",   3000.0,      2500.,   3500.,   "GeV")
+sigma = ROOT.RooRealVar("sigma", "sigma",  250.0,     100.,  400.,   "GeV")
+pdf = ROOT.RooGaussian("pdf", "pdf", var, mean, sigma)
+
+# run the fit
+pdf.fitTo(datahist, ROOT.RooFit.Range("r1"))
+
+## make plots
+tsize = 0.045
+
+ROOT.gStyle.SetPadLeftMargin(0.14)
+ROOT.gStyle.SetPadRightMargin(0.04)
+c = ROOT.TCanvas("c", "c", 800, 1000)
+c.SetLeftMargin(0.15)
+c.SetRightMargin(0.05)
+padsize1 = 0.67
+padsize2 = 0.33
+paddown = ROOT.TPad('bottompad', 'bottompad', 0.0, 0.0, 1, padsize2)
+padtop = ROOT.TPad('toppad', 'toppad', 0.0, padsize2, 1, 1)
+padtop.SetTopMargin(0.05*0.667/(padsize1*padsize1))
+padtop.SetBottomMargin(0.012/padsize1)
+paddown.SetTopMargin(0.010/padsize2)
+paddown.SetBottomMargin(0.13/padsize2)
+paddown.SetGridy(1)
+c.cd()
+paddown.Draw()
+padtop.Draw()
+
+padtop.cd()
+padtop.SetLogy()
+frame = var.frame()
+frame.GetXaxis().SetTitle("energy [ADCCount]")
+datahist.plotOn(frame)
+pdf.plotOn(frame)
+frame.SetTitle("EMCal Test beam")
+frame.GetXaxis().SetTitleSize(0.)
+frame.GetXaxis().SetLabelSize(0.)
+frame.GetYaxis().SetRangeUser(1.0, datahist.sumEntries())
+frame.GetYaxis().SetLabelSize(tsize)
+frame.GetYaxis().SetTitleSize(tsize*1.25)
+frame.GetYaxis().SetTitleOffset(1.1)
+frame.Draw()
+
+chi2 = frame.chiSquare()
+ndf = frame.GetNbinsX() - 2
+latex = ROOT.TLatex()
+latex.SetNDC()
+latex.SetTextColor(1)
+latex.SetTextSize(tsize)
+latex.SetTextFont(42)
+latex.DrawLatexNDC(0.20, 0.80, "#chi^{2}/ndf = %.2f" % (chi2))
+latex.DrawLatexNDC(0.20, 0.75, "#mu = %.2f #pm %.2f" % (mean.getVal(), mean.getError()))
+latex.DrawLatexNDC(0.20, 0.70, "#sigma = %.2f #pm %.2f" % (sigma.getVal(), sigma.getError()))
+latex.DrawLatexNDC(0.20, 0.65, "#sigma / #mu = %.1f%%" % (sigma.getVal()/mean.getVal()*100.))
+frame.addObject(latex)
+
+# make pull histograms
+hpull = frame.pullHist()
+frame2 = var.frame()
+frame2.SetTitle("")
+frame2.addPlotable(hpull, "P")
+frame2.GetXaxis().SetTitle("Energy [ADCCount]")
+frame2.GetYaxis().SetTitle("Pull")
+frame2.GetYaxis().CenterTitle()
+frame2.GetYaxis().SetNdivisions(505)
+frame2.GetYaxis().SetLabelSize(tsize*2)
+frame2.GetYaxis().SetTitleSize(tsize*2.5)
+frame2.GetYaxis().SetTitleOffset(0.5)
+frame2.GetXaxis().SetTitleOffset(1.2)
+frame2.GetXaxis().SetLabelSize(tsize*2)
+frame2.GetXaxis().SetTitleSize(tsize*2.5)
+
+paddown.cd()
+frame2.Draw()
+
+c.SaveAs("fit.pdf")
+c.SaveAs("fit.png")
+
+print("nevents: ", h.Integral(0, h.GetNbinsX()+1))
